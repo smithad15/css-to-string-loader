3250e87212e7702d48bd7e706f234a56e93a2f55
Andrew Smith <smithad15@gmail.com>
2016-05-30 22:27:46 -0400

    Add initial implementation

---
+++ index.js
@@ -0,0 +1,24 @@
+var loaderUtils = require('loader-utils');
+
+module.exports = function() { }
+
+module.exports.pitch = function(remainingRequest) {
+  if (this.cacheable) {
+    this.cacheable();
+  }
+  return [
+    "// css-to-text-loader: transforms styles from css-loader to a string output",
+    "",
+    "// Get the styles",
+    "var styles = require(" + loaderUtils.stringifyRequest(this, "!!" + remainingRequest) + ");",
+    "",
+    "if (typeof styles === 'string') {",
+    "// Return an existing string",
+    "module.exports = styles;",
+    "} else {",
+    "// Call the custom toString method from css-loader module",
+    "module.exports = styles.toString();",
+    "}"
+  ].join('\n');
+}
+
